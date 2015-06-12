4acb98013978e5d1451b6f65d38fa7abbbf71222
Leonardo Sala <leonardo.sala@psi.ch>
2015-06-12 06:00:30 -0700

    few fixes

---
+++ lh60/plot_multiple_emissions.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Plots emission lines from multiple runs
+It applies (if wanted):
++ ADU thresholds
++ dark background corrections
++ bad pixels map
++ ROIs
+"""
+
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+import h5py
+
+import sys
+XPP_LIB = "../xpp/utilities/"
+sys.path.append(XPP_LIB)
+import plot_utilities as pu
+from images_processor import ImagesProcessor
+import xpp_utilities as xpp
+
+## Runs to be analyzed
+#runs = [94, 95, 96, 89]
+#runs = [77, 81, 84, 79, 83]
+#runs = [110, 103, 107, 106, 108]
+#runs = [127, 128, ]
+#runs = [130, 131, 132, 133, 135, 136, 137, 138, 139, 140]
+runs = [152, 153, 154, 155, 156, 157]
+
+#DIR = "/reg/d/psdm/XPP/xpph6015/hdf5/"
+DIR = "/reg/d/psdm/xpp/xpph6015/ftc/hdf5/"
+
+## ROIs
+#roi0 = [[147, 155], [0, 391]]
+#roi1 = [[155, 172], [0, 391]]
+#roi0 = [[0, 388], [147, 155]]
+#roi1 = [[0, 388], [155, 172]]
+#roi0 = [[0, 388], [130, 190]]
+#roi1 = [[0, 388], [130, 190]]
+#roi0 = [[0, 388], [157, 168]]
+#roi1 = [[0, 388], [165, 180]]
+# from run 127
+roi0 = [[0, 388], [150, 160]]
+roi1 = [[0, 388], [105, 124]]
+
+# events per run to be analyzed
+n_events = 100
+
+ip = ImagesProcessor()
+# the dataset path common to everything:
+dset_main = "/Configure:0000/Run:0000/CalibCycle:0000/"
+
+spectra0 = []
+spectra1 = []
+image_labels = []
+images0 = []
+images1 = []
+
+# dark corrections
+dark_f = h5py.File("data/dark_run0030.h5", "r")
+dark0 = dark_f['CsPad0/mean'][:]
+dark1 = dark_f['CsPad1/mean'][:]
+
+# masking bad pixels
+bad_f = h5py.File("data/bad_pixels_r129_gt4.h5", "r")
+bad_pixel_mask0 = bad_f['CsPad0/bad_pixel_mask'][:]
+bad_pixel_mask1 = bad_f['CsPad1/bad_pixel_mask'][:]
+
+for run in runs:
+    fname = DIR + "xpph6015-r%04d.h5" % run  # DIR + "xpph6015-r0005.h5"
+    image_labels.append("Run %d" % run)
+    print "Running on %s" %fname
+    
+    # Starting the Images Processor for CsPad #0
+    dset_name = "CsPad2x2::ElementV1/XppGon.0:Cspad2x2.0"
+    ip.set_dataset(dset_main + dset_name)
+    # addind the analysis
+    ip.add_preprocess("correct_bad_pixels", args={"mask": bad_pixel_mask0})
+    ip.add_preprocess("subtract_correction", args={'sub_image': dark0})
+    ip.add_preprocess("set_thr", args={"thr_low": 25})
+    ip.add_preprocess("set_roi", args={'roi': roi0})
+    ip.add_analysis('get_mean_std', )
+    # special image iterator (CsPad140 needs some special care)
+    ip.set_images_iterator('images_iterator_cspad140')
+    # run the analysis
+    results0 = ip.analyze_images(fname, n=n_events)
+    
+    dset_name = "CsPad2x2::ElementV1/XppGon.0:Cspad2x2.1"
+    ip.set_dataset(dset_main + dset_name)
+    ip.add_preprocess("correct_bad_pixels", args={"mask": bad_pixel_mask1})
+    ip.add_preprocess("subtract_correction", args={'sub_image': dark1})
+    ip.add_preprocess("set_thr", args={"thr_low": 22})
+    ip.add_preprocess("set_roi", args={'roi': roi1})
+    # addind the analysis
+    ip.add_analysis('get_mean_std', )
+    # special image iterator (CsPad140 needs some special care)
+    ip.set_images_iterator('images_iterator_cspad140')
+    # run the analysis
+    results1 = ip.analyze_images(fname, n=n_events)
+    
+    # plot the results
+    images_mean0 = results0["get_mean_std"]["images_mean"]
+    images_mean1 = results1["get_mean_std"]["images_mean"]
+    
+    images0.append(images_mean0)    
+    images1.append(images_mean1)
+    
+    # get data from ipmb0
+    fname = '/reg/d/psdm/xpp/xpph6015/ftc/hdf5/xpph6015-r%04d.h5' % run
+    df_i0 = xpp.get_scalar_data(fname, "Ipimb::DataV2/XppEnds_Ipm0/data", n_events=n_events)
+    #df_i0 = xpp.get_scalar_data(fname, "Bld::BldDataFEEGasDetEnergyV1/FEEGasDetEnergy/data", n_events=n_events)
+    # using IPM0
+    r_norm = 1./ df_i0["Ipimb::DataV2/XppEnds_Ipm0/data.channel2"][0:n_events].sum()
+    # Using Gas Monitor
+    #r_norm = df_i0["Bld::BldDataFEEGasDetEnergyV1/FEEGasDetEnergy/data.f_11_ENRC"][0:n_events].sum()
+    #spectra0.append(images_mean0.sum(axis=1) / float(r_norm))
+    #spectra1.append(images_mean1.sum(axis=1) / float(r_norm))
+    # Using the max
+    spectra0.append(images_mean0.sum(axis=1) / float(images_mean0.sum(axis=1).max()))
+    spectra1.append(images_mean1.sum(axis=1) / float(images_mean1.sum(axis=1).max()))
+    
+
+# plottting everything+-
+plt.figure()
+for i, run in enumerate(runs):
+    plt.plot(spectra0[i], label=image_labels[i])
+plt.legend(loc="best")
+
+plt.figure()
+for i, run in enumerate(runs):
+    plt.plot(spectra1[i], label=image_labels[i])
+
+plt.legend(loc="best")
+plt.show()
---
+++ lh60/plot_spectrometers.py
@@ -0,0 +1,111 @@
+"""
+Simple script to plot FEE spectrometer data
+"""
+
+
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+import pandas as pd
+import imp
+from scipy.ndimage.measurements import center_of_mass
+
+
+# import dictionary with useful XPP datasets
+dset_names = imp.load_source('dset_names', 'xpp_datasets.py').dset_names
+
+# load xpp libraries
+import sys
+XPP_LIB = "../xpp/utilities/"
+sys.path.append(XPP_LIB)
+import plot_utilities as pu
+from images_processor import ImagesProcessor
+import xpp_utilities as xpp
+
+# checking arguments
+if len(sys.argv) != 2:
+    print "Usage: %s run_number" % sys.argv[0]
+    sys.exit(-1)
+
+# data filename
+#DIR = "/reg/d/psdm/xpp/xpph6015/hdf5/"
+# "Monitor datasets", without FEE images
+DIR = "/reg/d/psdm/xpp/xpph6015/ftc/hdf5/"
+run = int(sys.argv[1])
+fname = DIR + "xpph6015-r%04d.h5" % run  # DIR + "xpph6015-r0005.h5"
+print "Running on file %s" %fname
+
+f = h5py.File(fname, 'r')
+calib_cycle = 0
+
+# the root dataset
+main_dsetname = "/Configure:0000/Run:0000/CalibCycle:%04d/" % calib_cycle
+
+image_label = "Run %d, Calib %d" % (run, calib_cycle)
+
+### Getting SASE spectra from FEE spectrometer
+# getting the dataset and tags
+fee_spectr, fee_tags = xpp.get_data_with_tags(fname, dset_names["FEE_spectr_hproj"], calib_cycle=calib_cycle)
+fee_spectr = fee_spectr[:].astype('int32')
+#down_spectr, down_tags = xpp.get_data_with_tags(fname, dset_names["Downstream_spectr_img"] + "/image")
+fee_imgs = f[main_dsetname + dset_names["FEE_spectr_img"] + "/image"]
+down_imgs = f[main_dsetname + dset_names["Downstream_spectr_img"] + "/image"]
+
+
+# Starting the Images Processor for CsPad #0
+ip = ImagesProcessor()
+#dset_name = dset_names["FEE_spectr_img"]  #"CsPad2x2::ElementV1/XppGon.0:Cspad2x2.0"
+# setting the dataset
+#ip.set_dataset(main_dsetname + dset_name)
+# addind the analysis
+#ip.add_analysis('get_mean_std')
+#ip.add_analysis('get_projection')
+# special image iterator (CsPad140 needs some special care)
+#ip.set_images_iterator('images_iterator_cspad140')
+# run the analysis
+#results = ip.analyze_images(fname, )
+
+dset_name = dset_names["Downstream_spectr_img"]  #"CsPad2x2::ElementV1/XppGon.0:Cspad2x2.0"
+ip.set_dataset(main_dsetname + dset_name)
+ip.add_preprocess('set_roi', args={'roi': [[0, 1024], [440, 580]]})
+ip.add_analysis('get_mean_std')
+ip.add_analysis('get_projection', args={"axis": 1})
+results_down = ip.analyze_images(fname, )
+
+#pu.plot_image_and_proj(down_spectr[:].sum(axis=0), title="Downstream spectr %s (sum)" % run)
+
+tags_mask = np.in1d(results_down["tags"], fee_tags)
+down_filtered = results_down['get_projection']['spectra'][tags_mask]
+#fee_spectr = results['get_projection']['spectra']
+plt.figure()
+plt.title("FEE img proj, %s" % image_label)
+for i in range(3):
+    plt.plot(fee_spectr[i])
+
+plt.figure()
+plt.title("Down img proj, %s" % image_label)
+for i in range(3):
+    plt.plot(down_filtered[i])
+
+plt.figure()
+plt.title("FEE spectra sum, %s" % image_label)
+plt.plot(fee_spectr.sum(axis=0))
+
+plt.figure()
+plt.title("Downstream spectra sum, %s" % image_label)
+plt.plot(down_filtered.sum(axis=0))
+
+
+pu.plot_image_and_proj(fee_imgs[0], title=("FEE %s" % image_label))
+pu.plot_image_and_proj(down_imgs[1], title=("Downstream spectrometer %s" % image_label))
+
+#down_sp = results_down['get_projection']['spectra'][18]
+#fee_sp = results['get_projection']['spectra'][0]
+#down_sp = down_sp - down_sp[0]
+#fee_sp = fee_sp - fee_sp[0]
+#down_energy = 8381. - 0.108 * np.arange(1, down_sp.shape[0] + 1)
+#fee_energy = 8365. - 0.04 * np.arange(1, fee_sp.shape[0] + 1)
+
+plt.show()
+
