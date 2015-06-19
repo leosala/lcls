a8430e60d558d1bfc68665ce6f8766a3eb10ff23
Leonardo Sala <leonardo.sala@psi.ch>
2015-06-19 02:10:27 -0700

    better scripts placement

---
+++ lh60/load_defaults.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 26 16:47:06 2015
+
+@author: sala
+"""
+import h5py
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+import sys
+import os
+XPP_LIB = "../XPP/utilities/"
+sys.path.append(os.getcwd() + "/" + XPP_LIB)
+
+import hdf5_utilities as h5u
+from images_processor import ImagesProcessor
+import xpp_utilities as xpp
+import plot_utilities as pu
+
