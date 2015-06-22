6d6973f8f9df76de5a24e02a60d8caa3c63146e0
Leonardo Sala <leonardo.sala@psi.ch>
2015-06-22 01:28:47 -0700

    bugfix

--- lh60/get_fee_spectrometer_response.py
+++ lh60/get_fee_spectrometer_response.py
@@ -3,6 +3,7 @@
 This script runs over a particular set of runs, used to calibrate the FEE spectrometer, and
 then create a response function
 """
+from __future__ import division
 
 import sys
 XPP_LIB = "../xpp/utilities/"
@@ -17,7 +18,7 @@ import numpy as np
 DIR = "/reg/d/psdm/xpp/xpph6015/ftc/hdf5/"
 
 # ordert of the response polynomial to be fitted
-poly_order = 3
+poly_order = 5
 
 # x, y are the points we want to fit for the response envelope. Currently
 # they are fitted with the max value
@@ -27,11 +28,15 @@ y = []
 # eventual boundary conditions
 x.append(0)
 y.append(0)
-#x.append(2048)
-#y.append(0)
+x.append(2048)
+y.append(0)
 
 envelope = None
 
+s_x = x
+s_y = y
+
+spectra_sum = None
 plt.figure()
 # running on all runs ranging from 19 till 26
 for run in range(19, 27):
@@ -41,10 +46,26 @@ for run in range(19, 27):
     df = xpp.get_scalar_data(fname, dset_names["FeeGasDet"])
     # getting I0
     i0 = np.array(df["FEEGasDetEnergy.f_11_ENRC"].tolist())
+    # IPM0 CH2 - need to use a different method, as it is composite data
+    ipm0, ipm0_tags = xpp.get_data_with_tags(fname, dset_names["IPM0"])
+    i0_ch2 = ipm0["channel"][:, 2]
+
     # Getting FEE spectra
     fee_spectra, fee_tags = xpp.get_data_with_tags(fname, dset_names["FEE_spectr_hproj"])
     # normalization
-    spectra = fee_spectra[:].astype('int32').sum(axis=0) / i0.sum()
+    spectra = fee_spectra[:].astype('int32').sum(axis=0) / i0_ch2.sum(axis=0)
+    spectra_avg = np.max(spectra)
+    spectra_std = spectra.std()
+    if spectra_sum is None:
+        spectra_sum = spectra.copy()
+    else:
+        spectra_sum += spectra
+
+    s_t = np.where(np.isclose(spectra, spectra_avg, atol=500) == True)
+    s_y.append(spectra_avg)
+    idx = (np.abs(spectra - spectra_avg)).argmin()
+    s_x.append(idx)
+    print s_x, s_y
     # filling x, y
     y.append(spectra.max())
     t = np.where(spectra == y[-1])
@@ -53,13 +74,18 @@ for run in range(19, 27):
     plt.plot(spectra, label="Run %d" %run)
 
 # creating the envelope
-envelope = np.poly1d(np.polyfit(x, y, poly_order))
+pol_fit, pol_cov = np.polyfit(x, y, poly_order, cov=True)
+envelope = np.poly1d(pol_fit)
 envelope_values = envelope(range(spectra.shape[0]))
 
+plt.plot(range(spectra.shape[0]), envelope_values.max() * spectra_sum / spectra_sum.max(),  'k:', linewidth=2, label="normalized sum")
+plt.plot(s_x, s_y, 'o', label="points for the fit")
 print "\n## Response Polynomial"
 print envelope
+print "\n# Covariance Matrix"
+print pol_cov
 
 plt.plot(range(spectra.shape[0]), envelope_values, 'k', linewidth=2, label="envelope")
 plt.legend()
-plt.title("Runs 19-27, FEE Spectrometer, summed up")
+plt.title("Runs 19-27, FEE Spectrometer spectra")
 plt.show()
